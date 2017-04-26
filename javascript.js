daabdb4b6488b907c1763defd5860206637dd4f4
Tung Dao <me@tungdao.com>
2017-04-26 21:04:12 +0700

    add flexslider

--- javascript.js
+++ javascript.js
@@ -1,11 +1,21 @@
-var menu = document.getElementsByClassName('menu-container');
-var search = document.getElementsByClassName('search-container');
+document.addEventListener('DOMContentLoaded', function() {
+  var menu = document.getElementsByClassName('menu-container');
+  var search = document.getElementsByClassName('search-container');
 
-function toggleMenu(x) {
-  x.classList.toggle("is-active");
- menu[0].classList.toggle("dn");
-}
+  function toggleMenu(x) {
+    x.classList.toggle("is-active");
+    menu[0].classList.toggle("dn");
+  }
 
-function toggleSearch(){
-  search[0].classList.toggle("dn");
-}
+  function toggleSearch(){
+    search[0].classList.toggle("dn");
+  }
+})
+
+$(window).load(function() {
+
+  $('.flexslider').flexslider({
+    animation: 'slide'
+  })
+
+})
