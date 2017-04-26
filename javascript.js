a17905be9492528cfbdd99a64a9c836003e5427a
Bau Tran <bautran1911@gmail.com>
2017-04-26 14:06:46 +0700

    Fix Menu and search

--- javascript.js
+++ javascript.js
@@ -1,3 +1,11 @@
+var menu = document.getElementsByClassName('menu-container');
+var search = document.getElementsByClassName('search-container');
+
 function toggleMenu(x) {
   x.classList.toggle("is-active");
+ menu[0].classList.toggle("dn");
+}
+
+function toggleSearch(y){
+  search[0].classList.toggle("dn");
 }
